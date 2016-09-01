079a0b559ededf6b3592a271ba3630382e51adad
Craig Doremus <cdoremus@gmail.com>
2016-09-01 16:33:03 -0400

    Added ability to pull messge name from querystring

--- src/express_hello_world/ts/index.ts
+++ src/express_hello_world/ts/index.ts
@@ -8,7 +8,11 @@
  * node index.js
  *
  * Browse to:
- * http://localhost:3000/hello
+ * http://localhost:3000/hello?name=Craig
+ *
+ * In this case, 'Hello Craig' will be displayed. If no query
+ * parameter is used, then 'Hello World' will be displayed, which
+ * has been added to the request by the create function.
  *
  */
 import * as express from 'express';
@@ -19,12 +23,16 @@ let app: express.Express = express();
 
 // Use a middleware function defined in middleware.js
 // The use() function inserts the function in the middleware pipeline
-app.use(create('James'));
+app.use(create('World'));
 
 app.get('/hello', (request: ExtendedRequest, response: express.Response) => {
-
+    // Get value of name from query string parameter
+    let name: string = request.query.name;
+    if (!name) {
+        name = request.user.name;
+    }
     // create() adds user property
-    response.send(`Hello ${request.user.name}`); //passing a string assumes HTML output,
+    response.send(`Hello ${name}`); //passing a string assumes HTML output,
                       // passing a JavaScript object assumes you want to send JSON output
 });
 
