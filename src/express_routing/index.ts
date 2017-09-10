706d208ededab84c9063a5d7018d8ac3cbe7f99f
Craig Doremus <cdoremus@gmail.com>
2017-09-10 15:09:02 -0400

    Added static 'public' folder and /users endpoint for sending JSON.

--- src/express_routing/index.ts
+++ src/express_routing/index.ts
@@ -5,36 +5,46 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import {create} from './middleware';
 import { ExtendedRequest } from './interfaces';
+const users = require('./users.json');
 
 let app: express.Express = express();
 
 // Use a middleware function defined in middleware.js
 // The use() function inserts the function in the middleware pipeline
-// create() adds user property to the request
+// create() adds 'user' property to the request
+// whose type is the User interface
 app.use(create('Jane'));
 // Support JSON
 app.use(bodyParser.json());
 // Support HTML forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// serve an html file
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/views/index.html')
-});
+// set public folder as the app root
+app.use('/', express.static('public'));
 
 // serve a request with a query string parameter 'name'
 app.get('/hello', (request: ExtendedRequest, response: express.Response) => {
     let name: string = request.query.name;
     if (!name) {
-        name = request.user.name;
+    // ExtendedRequest extends express.Request
+    // to add User object to the request
+    name = request.user.name;
     }
     //passing a string assumes HTML output,
     response.send(`Hello ${name}<div><a href="/">Home</a></div>`);
 
-    // passing a JavaScript object assumes
-    // you want to send JSON output.
-    // response.send(request.user);
+});
+
+// sending back JSON
+// passing a JavaScript object assumes
+// you want to send JSON output.
+// In this case, the JSON is loaded
+// from the file users.json
+app.get('/users', (request: ExtendedRequest, response: express.Response) => {
+
+    response.send(users);
 
 });
+
 
 app.listen(3000, () => console.log('Application running on port 3000'));
