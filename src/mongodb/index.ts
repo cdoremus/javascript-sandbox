997f92364e6ee0696a16f1b6ef15359c34a3d101
Craig Doremus <cdoremus@gmail.com>
2017-09-16 12:17:26 -0400

    Invoking multiple saves wrapped in a promise

--- src/mongodb/index.ts
+++ src/mongodb/index.ts
@@ -1,17 +1,68 @@
-import * as mongoose from 'mongoose';
-import schema from './schema';
+import * as assert from 'assert';
+import * as console from 'console';
+import { Collection, createConnection } from 'mongoose';
+import { UserSchema } from './schema';
+import { User } from './domain-model';
+const users: User[] =  require('./users.json');
 
-const db = mongoose.createConnection('mongodb://localhost/states');
+const DB_URL = 'mongodb://localhost/persons'
+const COLLECTION_NAME = 'customers';
 
-const StatesModel = db.model('StatesModel', schema, 'states');
+// Use native promises
+// require('mongoose').Promise = global.Promise;
 
-StatesModel.find({'code': 'ME'}, 'name capital adjacent',
-  (err, resp) => {
-    if(err) {
-      console.error(`Problem with query:`, err);
-    } else {
-      console.log('Document found', resp);
+const db = createConnection(DB_URL);
+
+const UsersModel = db.model('UsersModel', UserSchema, COLLECTION_NAME);
+
+try {
+  saveUsers(UsersModel, users);
+  // saveUserDocument(UsersModel, users[2]);
+} catch(err) {
+  db.close();
+  console.error('ERROR: Saving users', err);
+  throw err;
+}
+db.close();
+
+
+function validateUserByUserId(id: number) {
+  let user;
+  // create a new connection
+  const db2 = createConnection(DB_URL);
+  const UsersModel2 = db2.model('UsersModel2', UserSchema, COLLECTION_NAME);
+  UsersModel2.findOne({'id': id}, (error, document ) => {
+    if (error) {
+      throw new Error(error);
     }
-    db.close();
+    user = document;
+    assert(user.id === id, `Failed to find user with id ${id}`);
+    console.log(`User with id ${id} found`);
+    db2.close();
+  });
+}
+
+function saveUserDocument(model, user) {
+  return new Promise((resolve, reject) => {
+    let entry = new model(user);
+    entry.save((err, document) => {
+      if (err) {
+        // throw new Error(err);
+        reject(err);
+      }
+      console.log(`MongoDB User created for user ${document.name} (${document.id})`);
+      // return document;
+      resolve(document);
+    });
+  });
+}
+
+function saveUsers(model, users) {
+    const promises = users.map(user => saveUserDocument(model, user));
+    console.log('PROMISES', promises);
+    Promise.all(promises);
   }
-);
+
+// function dropCustomersCollection(collection: Collection) {
+//   return collection.drop()
+// }
