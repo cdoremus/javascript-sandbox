b3421c28ee309e999a10cf3673a50a279ee05cf1
Craig Doremus <Craig.Doremus@cashstar.com>
2020-11-01 16:42:15 -0500

    Cleaned up transformObjectArray.js

--- src/reduce/transformObjectArray.js
+++ src/reduce/transformObjectArray.js
@@ -1,8 +1,15 @@
 
 /**
- * Transform an array of objects into an object whose key 
- * is the code property of each object
-*/
-arr = [{id:1, code: 'one'}, {id:2, code: 'two'}];
-newobj = arr.reduce((accum, item) => ({...accum, [item.code]: p }), {} )
-console.log(`New object: ${newobj}`);
+ * Use array.reduce() to transform an array 
+ * of objects into an object whose key is a
+ * is a property of each object ('code' in this example)
+ * 
+ * Usage node/deno:
+ * node transformObjectArray.js
+ * deno run transformObjectArray.js 
+ */
+const prop = 'code';
+const arr = [{id:1, [`${prop}`]: 'one'}, {id:2, code: 'two'}];
+const newobj = arr.reduce((accum, item) => ({...accum, [item[`${prop}`]]: item }), {} )
+console.log('New object: ', newobj);
+// { one: { id: 1, code: 'one' }, two: { id: 2, code: 'two' } }
